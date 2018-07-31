ba8d75c5c472343b4302948ffdd3493519a95bfe
EC2 Default User <ec2-user@ip-172-31-18-150.us-west-2.compute.internal>
2018-07-31 19:59:17 +0000

    convert format

---
+++ feature_engineering.py
@@ -0,0 +1,315 @@
+
+# coding: utf-8
+
+# # FE
+
+# In[ ]:
+
+
+import numpy as np
+import pandas as pd
+import os
+import gc
+import time
+
+
+# In[ ]:
+
+
+# One-hot encoding for categorical columns with get_dummies
+def one_hot_encoder(df, nan_as_category = True):
+    
+    '''
+    
+    return
+    ------
+    df: dataframe with OHE on categorical columns
+    new_columns: list of OHE name columns
+    
+    '''
+    
+    original_columns = list(df.columns)
+    categorical_columns = [col for col in df.columns if df[col].dtype == 'object'] # select categorical features
+    df = pd.get_dummies(df, columns= categorical_columns, dummy_na= nan_as_category)
+    new_columns = [c for c in df.columns if c not in original_columns]
+    return df, new_columns
+
+
+# In[ ]:
+
+
+# Preprocess application_train.csv and application_test.csv
+
+def application_train_test(df, test_df, nan_as_category = False, limit=None):
+    
+    # Merge
+    df = df.append(test_df).reset_index()
+    
+    # Optional: Remove 4 applications with XNA CODE_GENDER (train set)
+    df = df[df['CODE_GENDER'] != 'XNA']
+    
+    # Dummy 1 if missing value >= 35, 0 otherwise
+    # Median of the serie : 36
+    df['incomplete'] = 1
+    df.loc[df.isnull().sum(axis=1) < 35, 'incomplete'] = 0 
+
+    docs = [_f for _f in df.columns if 'FLAG_DOC' in _f]
+    live = [_f for _f in df.columns if ('FLAG_' in _f) & ('FLAG_DOC' not in _f) & ('_FLAG_' not in _f)]
+    
+    # NaN values for DAYS_EMPLOYED: 365.243 -> nan
+    df['DAYS_EMPLOYED'].replace(365243, np.nan, inplace= True)
+
+    inc_by_org = df[['AMT_INCOME_TOTAL', 'ORGANIZATION_TYPE']].groupby('ORGANIZATION_TYPE').median()['AMT_INCOME_TOTAL']
+
+    df['NEW_CREDIT_TO_ANNUITY_RATIO'] = df['AMT_CREDIT'] / df['AMT_ANNUITY']
+    df['NEW_CREDIT_TO_GOODS_RATIO'] = df['AMT_CREDIT'] / df['AMT_GOODS_PRICE']
+    df['NEW_DOC_IND_KURT'] = df[docs].kurtosis(axis=1)
+    df['NEW_LIVE_IND_SUM'] = df[live].sum(axis=1)
+    df['NEW_INC_PER_CHLD'] = df['AMT_INCOME_TOTAL'] / (1 + df['CNT_CHILDREN'])
+    df['NEW_INC_BY_ORG'] = df['ORGANIZATION_TYPE'].map(inc_by_org)
+    df['NEW_EMPLOY_TO_BIRTH_RATIO'] = df['DAYS_EMPLOYED'] / df['DAYS_BIRTH']
+    df['NEW_ANNUITY_TO_INCOME_RATIO'] = df['AMT_ANNUITY'] / (1 + df['AMT_INCOME_TOTAL'])
+    df['NEW_SOURCES_PROD'] = df['EXT_SOURCE_1'] * df['EXT_SOURCE_2'] * df['EXT_SOURCE_3']
+    df['NEW_EXT_SOURCES_MEAN'] = df[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].mean(axis=1)
+    df['NEW_SCORES_STD'] = df[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].std(axis=1)
+    df['NEW_SCORES_STD'] = df['NEW_SCORES_STD'].fillna(df['NEW_SCORES_STD'].mean())
+    df['NEW_CAR_TO_BIRTH_RATIO'] = df['OWN_CAR_AGE'] / df['DAYS_BIRTH']
+    df['NEW_CAR_TO_EMPLOY_RATIO'] = df['OWN_CAR_AGE'] / df['DAYS_EMPLOYED']
+    df['NEW_PHONE_TO_BIRTH_RATIO'] = df['DAYS_LAST_PHONE_CHANGE'] / df['DAYS_BIRTH']
+    df['NEW_PHONE_TO_BIRTH_RATIO_EMPLOYER'] = df['DAYS_LAST_PHONE_CHANGE'] / df['DAYS_EMPLOYED']
+    df['NEW_CREDIT_TO_INCOME_RATIO'] = df['AMT_CREDIT'] / df['AMT_INCOME_TOTAL']
+    
+    # Categorical features with Binary encode (0 or 1; two categories)
+    for bin_feature in ['CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY']:
+        df[bin_feature], uniques = pd.factorize(df[bin_feature])
+        
+    # Categorical features with One-Hot encode
+    df, cat_cols = one_hot_encoder(df, nan_as_category)
+    dropcolum=['FLAG_DOCUMENT_2','FLAG_DOCUMENT_4',
+    'FLAG_DOCUMENT_5','FLAG_DOCUMENT_6','FLAG_DOCUMENT_7',
+    'FLAG_DOCUMENT_8','FLAG_DOCUMENT_9','FLAG_DOCUMENT_10', 
+    'FLAG_DOCUMENT_11','FLAG_DOCUMENT_12','FLAG_DOCUMENT_13',
+    'FLAG_DOCUMENT_14','FLAG_DOCUMENT_15','FLAG_DOCUMENT_16',
+    'FLAG_DOCUMENT_17','FLAG_DOCUMENT_18','FLAG_DOCUMENT_19',
+    'FLAG_DOCUMENT_20','FLAG_DOCUMENT_21']
+    df= df.drop(dropcolum,axis=1)
+
+    return df
+
+
+# In[ ]:
+
+
+# Preprocess bureau.csv and bureau_balance.csv
+
+def bureau_and_balance(bureau, bb, nan_as_category = True, limit=None):
+    
+    # OHE cat feature
+    bb, bb_cat = one_hot_encoder(bb, nan_as_category)
+    bureau, bureau_cat = one_hot_encoder(bureau, nan_as_category)
+    
+    # Bureau balance: Perform aggregations and merge with bureau.csv
+    bb_aggregations = {'MONTHS_BALANCE': ['min', 'max', 'size']}
+    for col in bb_cat:
+        bb_aggregations[col] = ['mean']
+    bb_agg = bb.groupby('SK_ID_BUREAU').agg(bb_aggregations)
+    bb_agg.columns = pd.Index([e[0] + "_" + e[1].upper() for e in bb_agg.columns.tolist()])
+    bureau = bureau.join(bb_agg, how='left', on='SK_ID_BUREAU')
+    bureau.drop(['SK_ID_BUREAU'], axis=1, inplace= True)
+    del bb, bb_agg
+    gc.collect()
+    
+    # Bureau and bureau_balance numeric features
+    num_aggregations = {
+        'DAYS_CREDIT': [ 'mean', 'var'],
+        'DAYS_CREDIT_ENDDATE': [ 'mean'],
+        'DAYS_CREDIT_UPDATE': ['mean'],
+        'CREDIT_DAY_OVERDUE': ['mean'],
+        'AMT_CREDIT_MAX_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM': [ 'mean', 'sum'],
+        'AMT_CREDIT_SUM_DEBT': [ 'mean', 'sum'],
+        'AMT_CREDIT_SUM_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM_LIMIT': ['mean', 'sum'],
+        'AMT_ANNUITY': ['max', 'mean'],
+        'CNT_CREDIT_PROLONG': ['sum'],
+        'MONTHS_BALANCE_MIN': ['min'],
+        'MONTHS_BALANCE_MAX': ['max'],
+        'MONTHS_BALANCE_SIZE': ['mean', 'sum']
+    }
+    # Bureau and bureau_balance categorical features
+    cat_aggregations = {}
+    for cat in bureau_cat: cat_aggregations[cat] = ['mean']
+    for cat in bb_cat: cat_aggregations[cat + "_MEAN"] = ['mean']
+    
+    # GroupBy id and compute some aggregat
+    
+    bureau_agg = bureau.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    bureau_agg.columns = pd.Index(['BURO_' + e[0] + "_" + e[1].upper() for e in bureau_agg.columns.tolist()])
+    
+    # compute aggreagat in sous group : active credit and closed
+    
+    # Bureau: Active credits - using only numerical aggregations
+    active = bureau[bureau['CREDIT_ACTIVE_Active'] == 1]
+    active_agg = active.groupby('SK_ID_CURR').agg(num_aggregations)
+    active_agg.columns = pd.Index(['ACTIVE_' + e[0] + "_" + e[1].upper() for e in active_agg.columns.tolist()])
+    active_agg = active_agg.reset_index()
+    bureau_agg = bureau_agg.reset_index()
+    bureau_agg = bureau_agg.merge(active_agg, how='left', on='SK_ID_CURR')
+    del active, active_agg
+    gc.collect()
+    
+    # Bureau: Closed credits - using only numerical aggregations
+    closed = bureau[bureau['CREDIT_ACTIVE_Closed'] == 1]
+    closed_agg = closed.groupby('SK_ID_CURR').agg(num_aggregations)
+    closed_agg.columns = pd.Index(['CLOSED_' + e[0] + "_" + e[1].upper() for e in closed_agg.columns.tolist()])
+    closed_agg = closed_agg.reset_index()
+    bureau_agg = bureau_agg.merge(closed_agg, how='left', on='SK_ID_CURR')
+
+    return bureau_agg
+
+
+# In[ ]:
+
+
+# Preprocess previous_applications.csv
+
+def previous_applications(prev, nan_as_category = True, limit=None):
+        
+    prev, cat_cols = one_hot_encoder(prev, nan_as_category= True)
+    
+    # Days 365.243 values -> nan
+    prev['DAYS_FIRST_DRAWING'].replace(365243, np.nan, inplace= True)
+    prev['DAYS_FIRST_DUE'].replace(365243, np.nan, inplace= True)
+    prev['DAYS_LAST_DUE_1ST_VERSION'].replace(365243, np.nan, inplace= True)
+    prev['DAYS_LAST_DUE'].replace(365243, np.nan, inplace= True)
+    prev['DAYS_TERMINATION'].replace(365243, np.nan, inplace= True)
+    
+    # Add feature: value ask / value received percentage
+    prev['APP_CREDIT_PERC'] = prev['AMT_APPLICATION'] / prev['AMT_CREDIT']
+    
+    # Previous applications numeric features
+    num_aggregations = {
+        'AMT_ANNUITY': [ 'max', 'mean'],
+        'AMT_APPLICATION': [ 'max','mean'],
+        'AMT_CREDIT': [ 'max', 'mean'],
+        'APP_CREDIT_PERC': [ 'max', 'mean'],
+        'AMT_DOWN_PAYMENT': [ 'max', 'mean'],
+        'AMT_GOODS_PRICE': [ 'max', 'mean'],
+        'HOUR_APPR_PROCESS_START': [ 'max', 'mean'],
+        'RATE_DOWN_PAYMENT': [ 'max', 'mean'],
+        'DAYS_DECISION': [ 'max', 'mean'],
+        'CNT_PAYMENT': ['mean', 'sum'],
+    }
+    # Previous applications categorical features
+    cat_aggregations = {}
+    for cat in cat_cols:
+        cat_aggregations[cat] = ['mean']
+    
+    prev_agg = prev.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    prev_agg.columns = pd.Index(['PREV_' + e[0] + "_" + e[1].upper() for e in prev_agg.columns.tolist()])
+    
+    # Previous Applications: Approved Applications - only numerical features
+    approved = prev[prev['NAME_CONTRACT_STATUS_Approved'] == 1]
+    approved_agg = approved.groupby('SK_ID_CURR').agg(num_aggregations)
+    approved_agg.columns = pd.Index(['APPROVED_' + e[0] + "_" + e[1].upper() for e in approved_agg.columns.tolist()])
+    approved_agg = approved_agg.reset_index()
+    prev_agg = prev_agg.reset_index()
+    prev_agg = prev_agg.merge(approved_agg, how='left', on='SK_ID_CURR')
+    
+    # Previous Applications: Refused Applications - only numerical features
+    refused = prev[prev['NAME_CONTRACT_STATUS_Refused'] == 1]
+    refused_agg = refused.groupby('SK_ID_CURR').agg(num_aggregations)
+    refused_agg.columns = pd.Index(['REFUSED_' + e[0] + "_" + e[1].upper() for e in refused_agg.columns.tolist()])
+    refused_agg = refused_agg.reset_index()    
+    prev_agg = prev_agg.merge(refused_agg, how='left', on='SK_ID_CURR')
+
+    return prev_agg
+
+
+# In[ ]:
+
+
+# Preprocess POS_CASH_balance.csv
+
+def pos_cash(pos, nan_as_category = True, limit=None):
+    
+    pos, cat_cols = one_hot_encoder(pos, nan_as_category= True)
+    
+    # Features
+    aggregations = {
+        'MONTHS_BALANCE': ['max', 'mean', 'size'],
+        'SK_DPD': ['max', 'mean'],
+        'SK_DPD_DEF': ['max', 'mean']
+    }
+    for cat in cat_cols:
+        aggregations[cat] = ['mean']
+    
+    pos_agg = pos.groupby('SK_ID_CURR').agg(aggregations)
+    pos_agg.columns = pd.Index(['POS_' + e[0] + "_" + e[1].upper() for e in pos_agg.columns.tolist()])
+    
+    # Count pos cash accounts
+    pos_agg['POS_COUNT'] = pos.groupby('SK_ID_CURR').size()
+
+    return pos_agg
+
+
+# In[ ]:
+
+
+# Preprocess installments_payments.csv
+
+def installments_payments(ins, nan_as_category = True, limit=None):
+    
+    ins, cat_cols = one_hot_encoder(ins, nan_as_category= True)
+    
+    # Percentage and difference paid in each installment (amount paid and installment value)
+    ins['PAYMENT_PERC'] = ins['AMT_PAYMENT'] / ins['AMT_INSTALMENT']
+    ins['PAYMENT_DIFF'] = ins['AMT_INSTALMENT'] - ins['AMT_PAYMENT']
+    
+    # Days past due and days before due (no negative values)
+    ins['DPD'] = ins['DAYS_ENTRY_PAYMENT'] - ins['DAYS_INSTALMENT']
+    ins['DBD'] = ins['DAYS_INSTALMENT'] - ins['DAYS_ENTRY_PAYMENT']
+    ins['DPD'] = ins['DPD'].apply(lambda x: x if x > 0 else 0)
+    ins['DBD'] = ins['DBD'].apply(lambda x: x if x > 0 else 0)
+    
+    # Features: Perform aggregations
+    aggregations = {
+        'NUM_INSTALMENT_VERSION': ['nunique'],
+        'DPD': ['max', 'mean', 'sum','min','std' ],
+        'DBD': ['max', 'mean', 'sum','min','std'],
+        'PAYMENT_PERC': [ 'max','mean',  'var','min','std'],
+        'PAYMENT_DIFF': [ 'max','mean', 'var','min','std'],
+        'AMT_INSTALMENT': ['max', 'mean', 'sum','min','std'],
+        'AMT_PAYMENT': ['min', 'max', 'mean', 'sum','std'],
+        'DAYS_ENTRY_PAYMENT': ['max', 'mean', 'sum','std']
+    }
+    for cat in cat_cols:
+        aggregations[cat] = ['mean']
+    ins_agg = ins.groupby('SK_ID_CURR').agg(aggregations)
+    ins_agg.columns = pd.Index(['INSTAL_' + e[0] + "_" + e[1].upper() for e in ins_agg.columns.tolist()])
+    
+    # Count installments accounts
+    ins_agg['INSTAL_COUNT'] = ins.groupby('SK_ID_CURR').size()
+
+    return ins_agg
+
+
+# In[ ]:
+
+
+# Preprocess credit_card_balance.csv
+
+def credit_card_balance(cc, nan_as_category = True, limit=None):
+    
+    cc, cat_cols = one_hot_encoder(cc, nan_as_category= True)
+    
+    # General aggregations
+    cc.drop(['SK_ID_PREV'], axis= 1, inplace = True)
+    cc_agg = cc.groupby('SK_ID_CURR').agg([ 'max', 'mean', 'sum', 'var'])
+    cc_agg.columns = pd.Index(['CC_' + e[0] + "_" + e[1].upper() for e in cc_agg.columns.tolist()])
+    
+    # Count credit card lines
+    cc_agg['CC_COUNT'] = cc.groupby('SK_ID_CURR').size()
+
+    return cc_agg
+
